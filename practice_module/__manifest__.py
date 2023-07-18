35097142e7d795f8be21fcbb24e8a119fc14d23c
zoro2002 <ozmoaz@gmail.com>
2023-07-18 12:37:50 +0200

    added module practice

---
+++ practice_module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Practice Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Moses",
+    'category': 'Uncategorized',
+    'description': """
+    Description of your module.
+    """,
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
